542f8f7ff1162681cbf72d64ef7c54f2b59a04c7
Daniel Vargas <daniel-vargas@outlook.com>
2022-07-11 00:23:01 +0000

    Primeiro app

---
+++ webapp/test/integration/opaTests.qunit.js
@@ -0,0 +1,7 @@
+/* global QUnit */
+
+sap.ui.require(["AppFioriDaniel/appfioridaniel/test/integration/AllJourneys"
+], function () {
+	QUnit.config.autostart = false;
+	QUnit.start();
+});
---
+++ webapp/test/unit/AllTests.js
@@ -0,0 +1,5 @@
+sap.ui.define([
+	"AppFioriDaniel/appfioridaniel/test/unit/controller/Main.controller"
+], function () {
+	"use strict";
+});
